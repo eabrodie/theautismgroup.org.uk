fbc738188675c9cde1560b2c92f4d9bd8d9a356c
Eleanor Brodie <eleanorbrodie@Eleanors-MacBook-Pro.local>
2016-05-07 18:48:14 +0100

    Added 2 pages
    Added special interest groups and training pages

--- server.js
+++ server.js
@@ -17,9 +17,20 @@ app.get('/contact', function (req, res, next) {
   res.sendFile(__dirname + '/contact.html');
 });
 
+app.get('/groups', function (req, res, next) {
+  res.sendFile(__dirname + '/groups.html');
+});
+
+app.get('/training', function (req, res, next) {
+  res.sendFile(__dirname + '/training.html');
+});
+
 app.get('/style.css', function (req, res, next) {
   res.type('css');
-  res.sendFile(__dirname + '/style/style2.css', 'utf8');
+  res.send(
+    fs.readFileSync(__dirname + '/style/normalize.css', 'utf8') +
+    fs.readFileSync(__dirname + '/style/style2.css', 'utf8')
+  );
 });
 
 app.get('/logo-white-teal.svg', function (req, res, next) {
